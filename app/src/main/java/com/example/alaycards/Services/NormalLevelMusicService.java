0155dbdcd43ffe622350ce8b9862af4fe40958e9
CptZee <ajmission28@gmail.com>
2023-05-11 17:52:38 +0800

    Added a music service

---
+++ app/src/main/java/com/example/alaycards/Services/NormalLevelMusicService.java
@@ -0,0 +1,41 @@
+package com.example.alaycards.Services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+import com.example.alaycards.R;
+
+public class NormalLevelMusicService extends Service {
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer = MediaPlayer.create(this, R.raw.fx_normal);
+        mediaPlayer.setLooping(false);
+        mediaPlayer.setOnCompletionListener(mp -> mediaPlayer.start());
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer.start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
